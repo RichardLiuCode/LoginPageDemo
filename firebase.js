c059801958bd12c4a5d5d7d8af66e450d4cbbc6e
Richard Liu <richardadesigner@gmail.com>
2025-08-31 00:58:34 -0700

    set structure

---
+++ firebase.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD_2_Zm--BuJuX8uZ8-_It0faF3C6RSh8I",
+    authDomain: "login-system-test-c5db7.firebaseapp.com",
+    projectId: "login-system-test-c5db7",
+    storageBucket: "login-system-test-c5db7.firebasestorage.app",
+    messagingSenderId: "878181568417",
+    appId: "1:878181568417:web:69318e9bdf65787fdac5e0",
+    measurementId: "G-PE6Q3JY2WT"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var auth = firebase.auth()
